ec2f0dab0803576af2461eaab5cb15feef0ecc91
Hu Xinpeng <19384512+cloudstalker@users.noreply.github.com>
2018-12-20 00:17:23 +0800

    navBar responsive
    strange problem solved: use js/jquery instead of css

--- common_layout.js
+++ common_layout.js
@@ -1,12 +1,38 @@
 // Hamburger Menu Actions
-function menuMouseHover(){
+function menuClicked(){
     var lists = document.getElementsByTagName("li");
     for(var element of lists){
         if(element.style.display == "block"){
             element.style.display = "none";
+            $("#menuButtonImg").rotate({
+                angle: 90,
+                animateTo: 0,
+                duration: 200
+            });
         }
         else{
             element.style.display = "block";
+            $("#menuButtonImg").rotate({
+                angle: 0,
+                animateTo: 90,
+                duration: 200
+            });
+        }
+    }
+}
+
+function checkSize(){
+    if(document.body.clientWidth > 600){
+        var lists = document.getElementsByTagName("li");
+        for(var element of lists){
+            element.style.display = "block";
+            $("#menuButtonImg").rotate(0);
+        }
+    }
+    else{
+        var lists = document.getElementsByTagName("li");
+        for(var element of lists){
+            element.style.display = "none";
         }
     }
 }
